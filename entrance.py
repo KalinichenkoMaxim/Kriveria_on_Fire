c2f9708e006d49962c2486202ded4c872e2766c7
Майрам <Mairamscpsl@yandex.ru>
2026-01-11 22:51:05 +0300

    добавление обновленного экрана выбора стороны

--- entrance.py
+++ entrance.py
@@ -1,10 +1,13 @@
 import arcade
+from arcade import SpriteList
+from arcade.examples.drawing_primitives import scale, texture
 from arcade.gui import UIManager, UILabel
 from arcade.gui.widgets.layout import UIAnchorLayout, UIBoxLayout
 import random
 
 SCREEN_WIDTH = 1920
 SCREEN_HEIGHT = 1080
+
 
 class Enterance(arcade.Window):
     def __init__(self, width, height, title):
@@ -14,26 +17,34 @@ class Enterance(arcade.Window):
         self.sound = arcade.load_sound(f"soundtrack/{random.randint(1, 9)}.mp3")
         self.logo = arcade.load_texture("background_images/Kriveria on Fire.png")
         self.choice_texture = arcade.load_texture('background_images/choice.png')
+        self.texture_dnk = arcade.load_texture("background_images/DNK.png")
+        self.texture_gak = arcade.load_texture("background_images/GAK.png")
+        self.dnk_ico = arcade.Sprite(self.texture_dnk, scale=1.0)
+        self.gak_ico = arcade.Sprite(self.texture_gak, scale=1.0)
         self.manager = UIManager()
         self.manager.enable()
-        
+
         self.anchor_layout = UIAnchorLayout()
         self.box_layout = UIBoxLayout(vertical=True, space_between=10)
         self.show_choice_flag = False
-        
+
+        self.choosing_buttons = SpriteList()
+        self.choosing_buttons.append(self.dnk_ico)
+        self.choosing_buttons.append(self.gak_ico)
+
         self.setup_widgets()
-        
+
         self.anchor_layout.add(self.box_layout)
-        self.manager.add(self.anchor_layout)    
+        self.manager.add(self.anchor_layout)
 
     def setup_widgets(self):
-        arcade.play_sound(self.sound, volume=0.1)
+        arcade.play_sound(self.sound, volume=0.05)
         self.label = UILabel(
             x=1000,
             y=100,
-            text="Press any key...", 
-            font_size=50, 
-            text_color=arcade.color.WHITE, 
+            text="Press any key...",
+            font_size=50,
+            text_color=arcade.color.WHITE,
             width=300
         )
         self.manager.add(self.label)
@@ -43,28 +54,51 @@ class Enterance(arcade.Window):
         arcade.draw_texture_rect(self.background, arcade.rect.XYWH(960, 540, SCREEN_WIDTH, SCREEN_HEIGHT))
         arcade.draw_texture_rect(self.logo, arcade.rect.XYWH(500, 1000, 700, 50))
         if self.show_choice_flag:
-            arcade.draw_texture_rect(self.choice_texture, arcade.rect.XYWH(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, 1000, 500))
+            arcade.draw_texture_rect(self.texture_dnk,
+                                     arcade.rect.XYWH(SCREEN_WIDTH // 2 - 250, SCREEN_HEIGHT // 2, 300, 300))
+            arcade.draw_texture_rect(self.texture_gak,
+                                     arcade.rect.XYWH(SCREEN_WIDTH // 2 + 250, SCREEN_HEIGHT // 2, 300, 300))
+            arcade.draw_text(
+                "Выберете сторону для игры",
+                SCREEN_WIDTH // 2,
+                SCREEN_HEIGHT - 200,
+                arcade.color.BLACK,
+                font_size=50,
+                anchor_x="center",
+                anchor_y="top"
+            )
+
         self.manager.draw()
-    
+
     def on_key_press(self, key, modifiers):
         if key:
             if self.label:
                 self.manager.remove(self.label)
                 self.label = None
-                self.show_choice_flag = True
+                self.show_choice_flag = self.choosing_text = UILabel(
+                    x=SCREEN_WIDTH // 2,
+                    y=SCREEN_HEIGHT - 200,
+                    text="Выберете сторону для игры",
+                    font_size=50,
+                    text_color=arcade.color.WHITE,
+                    width=300
+                )
             return
-        #ZZZZZ
+
         if key == arcade.key.F4 and (modifiers & arcade.key.MOD_ALT):
             arcade.close_window()
             return
+
 
 def setup_game(width=800, height=600, title="Kriveria on Fire"):
     game = Enterance(width, height, title)
     return game
 
+
 def main():
     game = setup_game(SCREEN_WIDTH, SCREEN_HEIGHT, "Kriveria on Fire")
     arcade.run()
+
 
 if __name__ == "__main__":
     main()
